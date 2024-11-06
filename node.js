a5a9fe16cd248e5719e72b220d6ea694cbaa3d32
BTCapsule <115264270+BTCapsule@users.noreply.github.com>
2024-11-06 05:29:49 -0500

    Update node.js

--- node.js
+++ node.js
@@ -3,10 +3,27 @@ const cors = require('cors');
 const secureServer = require('./secureserver');
 const app = secureServer.app;
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// Add this route before starting the server
-app.get('/main', (req, res) => {
-    res.sendFile(path.join(__dirname, 'main.html'));
-});
+
+/*
+
+//USE THIS TO PROXY A PREBUILT APPS
+
+app.use('/', createProxyMiddleware({
+    target: 'http://localhost:4173', // iris-messenger local address
+    changeOrigin: true,
+    ws: true, // Enable WebSocket proxy
+    secure: false, 
+logLevel: 'silent',
+    onProxyReq: (proxyReq) => {
+        // Set mobile user agent
+        proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1');
+    }
+}));
+
+
+*/
+
 
 secureServer.start();
